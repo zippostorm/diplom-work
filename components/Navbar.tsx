e0619b18b5efdc91d48332f584431bcc609235e6
zippostorm <56356773+zippostorm@users.noreply.github.com>
2024-05-21 09:47:07 +0300

    fix navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -73,7 +73,7 @@ const Navbar = () => {
             <Link href="/upload">
               <button className='border-2 px-2 md:px-4 text-md font-semibold flex items-center gap-2'>
                 <IoMdAdd className='text-xl' /> {`  `}
-                <span className='hidden md:block'>Загрузити</span>
+                <span className='hidden md:block'>Завантажити</span>
               </button>
             </Link>
             {userProfile.image &&(
